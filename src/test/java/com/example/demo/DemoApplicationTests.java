4db02ec0a3fed6fd97a22ddc9f8f184cc48a8a20
Jasmin (Yaz) Amidzic <jasmin.amidzic@humanservices.gov.au>
2020-02-20 17:28:59 +1100

    adding logback support

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		LOGGER.info("Stating contextLoadsTest .... ");		
 	}
 
 }
